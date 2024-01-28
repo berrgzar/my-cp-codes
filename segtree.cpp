1874c61a77176d54f9fe392141013737ecb38939
berrgzar <131250342+berrgzar@users.noreply.github.com>
2024-01-28 17:21:41 +0300

    Update segtree.cpp

--- segtree.cpp
+++ segtree.cpp
@@ -1,26 +1,35 @@
-struct Segtree {                                                
-        int n;      
-        using T = int;
-
-        T join(const T& x, const T& y){
-            return x+y;
+struct Segtree{
+    int n;
+    using T = int;
+ 
+    T base = 0;
+    vector<T> arr;
+ 
+    T merge(const int &x, const int &y){
+        return (x+y);
+    }
+ 
+    Segtree(int n) : n(n), arr(2*n, base) {}
+ 
+    void update(int i, T x){
+        for (arr[i += n] += x; i > 1; i >>=1){
+            i >>= 1;
+            arr[i] = merge(arr[i<<1], arr[(i<<1) ^ 1]);
+            i <<= 1;
         }
+    }
+ 
+    T operator()(int l, int r){
+        T accl = base;
+        T accr = base;
+ 
+        for (l+=n, r+=n+1; l != r; l>>=1, r>>=1){
+            if (l&1) accl = merge(accl, arr[l++]);
+            if (r&1) accr = merge (arr[--r], accr);
+        }
+ 
+        return merge(accl, accr);
+    }
 
-        T base = 0;
-        vector<T> arr;                                        
-        Segtree(int n) : n(n), arr(2*n, base) {}                  
-        
-        void update(int i, int x) {                             
-                for (arr[i += n] = x; i > 1; i >>= 1)           
-                        arr[i>>1] = join(arr[i], arr[i^1]);      
-        }                                                       
-        T operator()(int l, int r) {                               
-                T accl = base;
-                T accr = base;                        
-                for (l += n, r += n+1; l != r; l >>= 1, r >>= 1) {
-                        if (l&1) accl = join(accl, arr[l++]);      
-                        if (r&1) accr = join(arr[--r], accr);      
-                }                                               
-                return join(accl, accr);                                     
-        }                                                       
-};  
+
+}; 
